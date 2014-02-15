94fabd398f88be0bb8b4f9e6579c35ad1d3adcc1
Justin Osterholt <shroder@gmail.com>
2014-02-15 06:47:46 -0800

    Debugging spring-security errors

--- src/com/hatraz/bucketlist/controller/LoginController.java
+++ src/com/hatraz/bucketlist/controller/LoginController.java
@@ -6,20 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.hatraz.bucketlist.model.User;
-import com.hatraz.bucketlist.service.UserDetailServiceImpl;
-
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.RequestToken;
 
 @Controller
 public class LoginController {
@@ -30,7 +20,7 @@ public class LoginController {
 	
 	@RequestMapping(value="/twitter-login", method=RequestMethod.GET)
 	public void twitterLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Twitter twitter = new TwitterFactory().getInstance();
+/*        Twitter twitter = new TwitterFactory().getInstance();
         request.getSession().setAttribute("twitter", twitter);
         try {
             StringBuffer callbackURL = request.getRequestURL();
@@ -46,11 +36,11 @@ public class LoginController {
 
         } catch (TwitterException e) {
             throw new ServletException(e);
-        }
+        }*/
 		return;
 	}
 	
-	@RequestMapping(value="/twitter-callback", method=RequestMethod.GET)
+/*	@RequestMapping(value="/twitter-callback", method=RequestMethod.GET)
 	public void twitterCallback(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Twitter callback");
         Twitter twitter = (Twitter) request.getSession().getAttribute("twitter");
@@ -66,14 +56,11 @@ public class LoginController {
   	      	// If user, then redirect to new page
   	      	// Add user to security context
             
-/*            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            authentication
-            
-*/
+            // Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         } catch (TwitterException e) {
             throw new ServletException(e);
         }
         response.sendRedirect(request.getContextPath() + "/");
 		return;
-	}
+	}*/
 }
